34877967dbcd3e5264aa87fbcea8004ee185e78b
Fredo-Ronan <fredocode06@gmail.com>
2023-12-14 16:49:12 +0700

    try different

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,46 +5,41 @@ import { RegisterPage } from "../pages/register/Register";
 import { VerificationSend } from "../pages/register/VerificationSend";
 import { VerifyPage } from "../pages/register/Verify";
 
-
 const router = createBrowserRouter([
-    {
-        path: "*",
-        element: <div>Route Not Found!</div>,
-    },
-    {
-        children: [
-            {
-                path: "/",
-                element: <LoginPage/> // Halaman Login
-            },
-            {
-                path: "/signup",
-                element: <RegisterPage/> // Halaman Sign Up/Register
-            },
-            {
-                path: "/verification_send/:email",
-                element: <VerificationSend/> // Halaman Pemberitahuan Untuk Verifikasi ke Email
-            },
-            {
-                path: "/verify",
-                element: <VerifyPage/> // Halaman Check Verification Key
-            }
-        ]
-    },
-    {
-        path: "/home",
-        element: (
-            <ProtectedRoutes>
-
-            </ProtectedRoutes>
-        )
-    }
+  {
+    path: "*",
+    element: <div>Route Not Found!</div>,
+  },
+  {
+    children: [
+      {
+        path: "/",
+        element: <LoginPage />, // Halaman Login
+      },
+      {
+        path: "/signup",
+        element: <RegisterPage />, // Halaman Sign Up/Register
+      },
+      {
+        path: "/verification_send/:email",
+        element: <VerificationSend />, // Halaman Pemberitahuan Untuk Verifikasi ke Email
+      },
+    ],
+  },
+  {
+    path: "/verify",
+    element: <VerifyPage />, // Halaman Check Verification Key
+  },
+  {
+    path: "/home",
+    element: <ProtectedRoutes></ProtectedRoutes>,
+  },
 ]);
 
 export const AppRouter = () => {
-    return (
-        <>
-            <RouterProvider router={router}/>
-        </>
-    )
-}
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
