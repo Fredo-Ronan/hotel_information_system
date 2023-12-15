73553f029826251f388d6752ef72bbd79185f9f3
Fredo-Ronan <fredocode06@gmail.com>
2023-12-15 23:53:42 +0700

    fix styling and add protected routes

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -2,17 +2,17 @@ import { useNavigate, Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 /* eslint-disable react/prop-types */
-export const ProtectedRoutes = ({ children }) => {
+const ProtectedRoutes = ({ children }) => {
   const navigate = useNavigate();
   const [token, setToken] = useState("");
-  
   useEffect(() => {
-    const tokenFromSession = sessionStorage.getItem("token");
-    setToken(tokenFromSession);
-    if (!tokenFromSession) {
+    const tokenDariSS = sessionStorage.getItem("token");
+    setToken(tokenDariSS);
+    if (!tokenDariSS) {
       navigate("/");
     }
   }, [navigate]);
-  
   return token && (children ? children : <Outlet />);
 };
+
+export default ProtectedRoutes;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,26 +1,72 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ProtectedRoutes } from "./ProtectedRoutes";
+import ProtectedRoutes from "./ProtectedRoutes";
 import { LoginPage } from "../pages/login/Login";
 import { RegisterPage } from "../pages/register/Register";
 import { VerificationSend } from "../pages/register/VerificationSend";
+import { UserLayout } from "../layouts/UserLayout";
+import { HomePage } from "../pages/home/home";
 
 const router = createBrowserRouter([
+  // Area Ketika User Login
   {
-    path: "/",
-    element: <LoginPage />, // Halaman Login
+    path: "/user",
+    element: (
+      <ProtectedRoutes>
+        <UserLayout />
+      </ProtectedRoutes>
+    ), // Halaman protected by ProtectedRoutes
+    children: [
+      {
+        path: "/user/profile",
+        element: <></>, // Halaman Profil
+      },
+      {
+        path: "/user/home",
+        element: <HomePage />, // Halaman Home
+      },
+      {
+        path: "/user/rooms",
+        element: <></>, // Halaman list kamar
+      },
+      {
+        path: "/user/rooms/booking",
+        element: <></>, // Halaman Booking Kamar
+      },
+      {
+        path: "/user/rooms/booking/payment",
+        element: <></>, // Halaman pembayaran booking kamar
+      },
+      {
+        path: "/user/contact",
+        element: <></>, // Halaman informasi kontak hotel
+      },
+    ],
   },
+
+  // Area ketika tidak ada user login
   {
-    path: "/signup",
-    element: <RegisterPage />, // Halaman Sign Up/Register
+    children: [
+      {
+        path: "/",
+        element: <LoginPage />, // Halaman Login
+      },
+      {
+        path: "/signup",
+        element: <RegisterPage />, // Halaman Sign Up/Register
+      },
+      {
+        path: "/verification_send/:email",
+        element: <VerificationSend />, // Halaman Pemberitahuan Untuk Verifikasi ke Email
+      },
+      {
+        path: "/home",
+        element: <HomePage />, // Halaman home tidak login
+      },
+    ]
   },
-  {
-    path: "/verification_send/:email",
-    element: <VerificationSend />, // Halaman Pemberitahuan Untuk Verifikasi ke Email
-  },
-  {
-    path: "/home",
-    element: <ProtectedRoutes />, // Halaman protected by ProtectedRoutes
-  },
+  
+
+  // Handle Route Not Found
   {
     path: "*",
     element: <div>Route Not Found!</div>,
