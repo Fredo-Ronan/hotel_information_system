fc16468bbba59edd917a3ac079d0ec0036c83d14
Fredo-Ronan <fredocode06@gmail.com>
2025-10-10 00:04:50 +0700

    dockerize

---
+++ clean_build.sh
@@ -0,0 +1,2 @@
+sudo docker compose down --rmi all --volumes --remove-orphans
+sudo docker compose up -d --build
--- src/api/apiIndex.jsx
+++ src/api/apiIndex.jsx
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 // Hosting Vercel Base URL
-export const BASE_URL = "https://the5stars-hotel-api.vercel.app";
+// export const BASE_URL = "https://the5stars-hotel-api.vercel.app";
 
 // Localhost Base URL
-// export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "http://127.0.0.1:8080";
 
 // Hosting Vercel Axios Config
-export const useAxios = axios.create({
-    baseURL: `${BASE_URL}/api/api`,
-});
+// export const useAxios = axios.create({
+//     baseURL: `${BASE_URL}/api/api`,
+// });
 
 // Localhost Axios Config
-// export const useAxios = axios.create({
-//     baseURL: `${BASE_URL}/api`,
-// });
+export const useAxios = axios.create({
+    baseURL: `${BASE_URL}/api`,
+});
