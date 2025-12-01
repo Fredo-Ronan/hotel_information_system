661a74c7f2a59db660f771394d72e2314d0bfb8d
Fredo Code <128687596+Fredo-Ronan@users.noreply.github.com>
2025-12-01 17:29:37 +0700

    Update BASE_URL for API endpoint

--- src/api/apiIndex.jsx
+++ src/api/apiIndex.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // Hosting Vercel Base URL
-// export const BASE_URL = "https://the5stars-hotel-api.vercel.app";
+export const BASE_URL = "https://his-api.fredoronan.web.id";
 
 // Localhost Base URL
-export const BASE_URL = "http://127.0.0.1:8080";
+// export const BASE_URL = "http://127.0.0.1:8080";
 
 // Hosting Vercel Axios Config
 // export const useAxios = axios.create({
